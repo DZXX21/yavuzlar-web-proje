5caf585f43f030d9330733135912d3306b2f7749
DZXX21 <91760460+DZXX21@users.noreply.github.com>
2023-07-21 14:24:35 +0300

    yama1
    1

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Formdan verileri alma
+    $fullname = $_POST['fullname-surname'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Veritabanı bağlantısı
+    $servername = "localhost";
+    $username = "kullanici_adi";
+    $password = "sifre";
+    $dbname = "veritabani_adi";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Bağlantı kontrolü
+    if ($conn->connect_error) {
+        die("Veritabanına bağlanılamadı: " . $conn->connect_error);
+    }
+
+    // Veritabanına veri ekleme
+    $sql = "INSERT INTO contact_form (fullname, email, subject, message) VALUES ('$fullname', '$email', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Mesajınız başarıyla gönderildi.";
+    } else {
+        echo "Hata: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Veritabanı bağlantısını kapatma
+    $conn->close();
+}
+?>
