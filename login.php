aeb19e131169fb212143917ac027ef2945e33ff5
DZXX21 <91760460+DZXX21@users.noreply.github.com>
2023-07-21 16:07:42 +0300

    php login
    1

--- login.php
+++ login.php
@@ -13,32 +13,23 @@ if ($conn->connect_error) {
     die("Veritabanına bağlanırken hata oluştu: " . $conn->connect_error);
 }
 
-// Form gönderildiğinde işleme başlayalım
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Formdan kullanıcı adı ve şifre alınması
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+// Kullanıcı adını alın
+$username = $_POST['username'];
 
-    // Veritabanında kullanıcıyı sorgulama
-    $sql = "SELECT * FROM users WHERE username = '$username'";
-    $result = $conn->query($sql);
+// Şifreyi alın ve güvenli bir şekilde şifreleyin (password_hash ile şifreleme)
+$password = $_POST['password'];
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    if ($result->num_rows == 1) {
-        $row = $result->fetch_assoc();
-        // Şifreyi doğrulayın (password_verify ile şifreli parolayı doğrulayın)
-        if (password_verify($_POST["password"], $row["password"])) {
-            // Giriş başarılı, kullanıcıyı ana sayfaya yönlendirin veya başka bir işlem yapın.
-            echo "Giriş başarılı!";
-            // Örnek olarak giriş başarılı olduğunda ana sayfaya yönlendirelim
-            header("Location: ana_sayfa.php");
-            exit();
-        } else {
-            // Giriş başarısız, kullanıcıyı login sayfasına geri yönlendirin veya hata mesajı gösterin.
-            echo "Kullanıcı adı veya şifre hatalı!";
-        }
-    } else {
-        // Kullanıcı adı bulunamadı, giriş başarısız
-        echo "Kullanıcı adı veya şifre hatalı!";
-    }
+// Admin kullanıcısını veritabanına ekleyin
+$sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashedPassword')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Admin kullanıcısı başarıyla oluşturuldu.";
+} else {
+    echo "Hata: " . $sql . "<br>" . $conn->error;
 }
+
+$conn->close();
+
+
 ?>
